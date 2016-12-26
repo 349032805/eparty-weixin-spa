6221ffb44fcefed783d3715d0051b006bc08ae91
349032805@qq.com <349032805@qq.com>
2016-12-26 13:40:17 +0800

    git commit

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,8 +23,6 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/AngularJS-Toaster/toaster.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-messages/angular-messages.js',
       // endbower
