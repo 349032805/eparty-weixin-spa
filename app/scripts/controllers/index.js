318b20ffc227cceb85e90efad005086959d4c45f
349032805@qq.com <349032805@qq.com>
2016-12-29 09:53:08 +0800

    git commit

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('eparty')
-  .controller('IndexCtrl', function ($scope,$rootScope) {
-  	 // setTimeout(hideLoad,3000);
-
-  	 // function hideLoad(){
+  .controller('IndexCtrl', function ($scope,$timeout,$rootScope) {
+  	 // setTimeout(function() {
   	 // 	angular.element(".load-flash").fadeOut();
-  	 // }
+    // }, 2000);
+
+
+  	 if(!$rootScope.showTimes){
+  	 	$timeout(function() {
+  	 		angular.element(".load-flash").fadeOut();
+        }, 2000);
+        $rootScope.showTimes = 1;
+  	}else{
+  		angular.element(".load-flash").hide();
+  	}
     
   });
