a96d8dc01d43a1ace6d61bb509f25fb58aac8e04
349032805@qq.com <349032805@qq.com>
2017-01-05 14:04:12 +0800

    git commit

--- app/scripts/controllers/waiterRanking.js
+++ app/scripts/controllers/waiterRanking.js
@@ -7,22 +7,26 @@ angular.module('eparty')
   	var list = [
    {
      "name": "张三",
+     "sex": 0,
      "score": 80,
      "date": 1483523354510
    },
    {
      "name": "尼古拉斯赵四",
+     "sex": 0,
      "score": 99,
      "date": 1483423254510
    },
    {
      "name": "王五",
      "score": 76,
+     "sex": 1,
      "date": 1483723454510
    },
     {
      "name": "田七",
      "score": 77,
+     "sex": 1,
      "date": 1483926354510
    }
   ];
